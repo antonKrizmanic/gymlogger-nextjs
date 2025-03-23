8b039f2a739c7bd7cdd605d89e6c3ca5d60b2098
Toni <anton.krizmanic94@gmail.com>
2025-03-23 21:11:54 +0100

    Update header.tsx

--- src/components/Auth/header.tsx
+++ src/components/Auth/header.tsx
@@ -5,7 +5,7 @@ interface HeaderProps {
 export const Header = ({ label }: HeaderProps) => {
     return(
         <div className="w-full flex flex-col gap-y-4 items-center justify-center">
-            <h1 className="text-3xl font-semibold">Gym Logger</h1>
+            <h1 className="text-3xl font-semibold">GymNotebook</h1>
             <p>{label}</p>
         </div>
     )
