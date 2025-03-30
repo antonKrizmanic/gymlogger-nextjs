6012450b57c6d58da850f11405912d9d55aca4a8
Toni <anton_krizmanic@hotmail.com>
2025-03-30 14:22:45 +0200

    m

--- src/api/services/dashboard-api-service.ts
+++
@@ -1,9 +0,0 @@
-import { BaseApiService } from './base-api-service';
-import { Endpoints } from '../endpoints';
-import { IDashboard } from '../../models/domain/dashboard';
-
-export class DashboardApiService extends BaseApiService {
-    public async getDashboard(): Promise<IDashboard> {
-        return this.get<IDashboard>(Endpoints.Dashboard.Base);
-    }
-} 
